9dac49f274d1af46fc3e8ff5ebf8db7b35563925
seandolinar <sean>
2017-12-04 00:04:17 -0500

    better trim

--- config.py
+++ config.py
@@ -6,13 +6,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
-database=url.path[1:],
+database=url.path[2:],
 user=url.username,
 password=url.password,
 host=url.hostname,
 port=url.port
 
-print(database)
 
 
 SQLALCHEMY_DATABASE_URI = "postgresql://" + str(user) + ":" + str(password) + "@" + str(host) + ":" + str(port) + "/" + str(database)

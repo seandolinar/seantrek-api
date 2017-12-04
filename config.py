5e12a4b7fe5d1be26c884976a1eb44a2873b461f
seandolinar <sean>
2017-12-03 23:44:07 -0500

    new config

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import urlparse
+import os
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+database=url.path[1:],
+user=url.username,
+password=url.password,
+host=url.hostname,
+port=url.port
+
+SQLALCHEMY_DATABASE_URI = "postgresql://" + user + ":" + password + "@" + host + ":" + port + "/" + database

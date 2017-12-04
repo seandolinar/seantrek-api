b84486f5ca2757ef824a757ff139414a6d063ea7
seandolinar <sean>
2017-12-04 00:14:29 -0500

    more config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
-database=url.path[2:]
+database=url.path[1:]
 user=url.username
 password=url.password
 host=url.hostname
